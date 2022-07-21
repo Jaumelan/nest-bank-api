d8e8848bb1c13c9c1f07c982519355ec9e988402
Jaime <jaume.lan@gmail.com>
2022-07-21 09:47:59 -0300

    module creation

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AccountsModule } from './accounts/accounts.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AccountsModule, TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
